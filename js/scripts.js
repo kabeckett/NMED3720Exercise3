5dbc472352d71a98eea69b13e43b612abb567ef4
Beckett <k.beckett@uleth.ca>
2019-03-04 11:22:20 -0700

    progress

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,29 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 // add the renderer element to the DOM
 document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry (20, 20, 20);
+// var material = new THREE.MeshLambertMaterial ({color: 0xfd59d7});
+
+var textureLoader = new THREE.textureLoader();
+
+textureLoader.crossOrigin = true;
+
+textureLoader.load('https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_960_720.jpg');
+
+texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 2, 2 );
+
+var material = new THREE.MeshLambertMaterial( {map: texture} );
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+var light = new THREE.PointLight(0xFFFF00);
+
+light.position.set(10, 0, 25);
+scene.add(light);
+
+cube.rotation.x = 0.45;
+cube.rotation.y = -0.25;
+
+cube.position.x = -30;
