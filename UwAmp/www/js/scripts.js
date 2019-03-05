1eca4b2ca94fcad768569aa3c90a875c74ca8f43
Kelly <k.beckett@uleth.ca>
2019-03-05 09:40:00 -0700

    added tweenlite library

--- UwAmp/www/js/scripts.js
+++ UwAmp/www/js/scripts.js
@@ -1,4 +1,5 @@
-var imgMaterial;
+
+// DECLARE VARIABLES
 var scene;
 var camera;
 var renderer;
@@ -7,14 +8,14 @@ var cubeGeometry;
 var icoGeometry;
 var color;
 var material;
+var imgMaterial;
 var textureLoader;
 var cube;
 var icosahedron;
 var render;
 var rotation;
 
-// window.onload = init;
-
+// Initialize the scene, camera, renderer
 function initScene() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -24,6 +25,7 @@ function initScene() {
 	camera.position.z = 100;
 };
 
+// Initialize the lightings and add them to the scene
 function initLighting() {
 	light1 = new THREE.PointLight( 0xFFFFFF );
 	light1.position.set( -20, 15, 25 );
@@ -36,15 +38,17 @@ function initLighting() {
 	scene.add( light2 );
 };
 
+// Initialize the shapes
 function initShapes() {
-// make geometry
+	// make geometry
 	cubeGeometry = new THREE.BoxGeometry ( 20, 20, 20 );
 	icoGeometry = new THREE.IcosahedronGeometry( 20, 0 );
 
-	// create material for the objects
-	// var material = new THREE.MeshNormalMaterial();
-	color = new THREE.Color( "#9370DB" );
+	// create materials for the objects
+	color = new THREE.Color( "#9370DB" ); // color: light purple
 	material = new THREE.MeshLambertMaterial( {color: color.getHex(), wireframe: true} );
+
+	// texture loader for image material
 	textureLoader = new THREE.TextureLoader();
 	textureLoader.crossOrigin = true;
 	textureLoader.load('tiles.jpg', function(texture) {
@@ -53,32 +57,38 @@ function initShapes() {
 		imgMaterial = new THREE.MeshLambertMaterial( {map: texture} );
 	});
 
+	// create the objects
 	cube = new THREE.Mesh( cubeGeometry, imgMaterial );
 	icosahedron = new THREE.Mesh( icoGeometry, material );
 
-
-	// add the cube to the scene
+	// add the objects to the scene
 	scene.add( cube );
 	scene.add( icosahedron );
 
+	// rotate the cube
 	cube.rotation.x = 0.45;
 	cube.rotation.y = -0.25;
 
+	// reposition the cube
 	cube.position.x = -30;
 
 	//tweenIcosohedron();
 
+	// rotate the icosohedron
 	icosahedron.rotation.x = 0.5;
 	icosahedron.rotation.y = 0.1;
 
+	// reposition the icosohedron
 	icosahedron.position.x = 25;
 };
 
+// render the scene
 function render() {
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
 };
 
+// animate icosahedron rotation
 function tweenIcosohedron() {
 	rotation = {x: Math.random()*3, y: Math.random()*3, z: Math.random()*3};
 	TweenLite.to(icosahedron.rotation, 1, {x: icosahedron.rotation.x, y: icosahedron.rotation.y, z: icosahedron.rotation.z, ease: Back.easeInOut, onComplete: tweenIcosohedron});
